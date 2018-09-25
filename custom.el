47dd2e9b5c80622395f8bedf52ab5f5f98adc401
Andy Pham <apham@walmartlabs.com>
2018-09-25 14:09:22 -0700

    Add macOS, Tmux and Emacs: Copy and Paste.

--- custom.el
+++ custom.el
@@ -1,3 +1,18 @@
+;; macOS, Tmux and Emacs: Copy and Paste.
+;; http://iancmacdonald.com/macos/emacs/tmux/2017/01/15/macOS-tmux-emacs-copy-past.html
+(defun copy-from-osx ()
+  "Use OSX clipboard to paste."
+  (shell-command-to-string "reattach-to-user-namespace pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  "Add kill ring entries (TEXT) to OSX clipboard.  PUSH."
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "reattach-to-user-namespace" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
 ;; Enable backup files.
 ;;(setq make-backup-files t)
 (setq make-backup-files nil) ; stop creating those backup~ files
